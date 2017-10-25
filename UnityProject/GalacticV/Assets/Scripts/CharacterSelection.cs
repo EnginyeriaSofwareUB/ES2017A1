4b427e84998f4d4c0a35f9003126a314204a61da
ShenYi24 <sossns524@gmail.com>
2017-10-25 16:22:34 +0200

    Menu Seleccio unitats en proces

---
+++ UnityProject/GalacticV/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour {
+
+	private GameObject[] character;
+
+	private void Start(){
+		character=new GameObject[transform.childCount];
+
+		for (int i = 0; i < transform.childCount; i++) {
+			character [i] = transform.GetChild (i).gameObject;
+		}
+
+		foreach (GameObject go in character) {
+			go.SetActive (false);
+		}
+
+		if (character [0])
+			character [0].SetActive (true);
+		
+	}
+
+
+}
