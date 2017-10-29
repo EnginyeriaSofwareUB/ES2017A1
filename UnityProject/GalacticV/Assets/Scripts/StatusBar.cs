35dc6b4660870ef46fc220444b84f294310c45de
Gabri <gabrikid96@gmail.com>
2017-10-29 21:12:47 +0100

    Primer disseny interficie

---
+++ UnityProject/GalacticV/Assets/Scripts/StatusBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+
+	[SerializeField]
+	private Text pointsText;
+
+	private GameObject currentBar;
+	private float ratio;
+	private float initialWidth;
+
+	private void Start()
+	{
+		currentBar = this.gameObject.transform.Find("image").gameObject;
+		initialWidth = (this.currentBar.transform as RectTransform).sizeDelta.x;
+	}
+
+	public void UpdateStatusBar(float currentPoints, float maxPoints)
+	{
+		RectTransform r = currentBar.transform as RectTransform;
+		float actualWidth = (initialWidth * currentPoints) / maxPoints;
+		r.sizeDelta = new Vector2(actualWidth, r.sizeDelta.y);
+		pointsText.text = currentPoints.ToString();
+	}
+}
